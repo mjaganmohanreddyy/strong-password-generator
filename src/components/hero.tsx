8c41c64b6d783f5e18acb42c44877370db6e6a7f
shekar <rajksreddy@gmail.com>
2024-10-04 13:59:55 +0530

    added

---
+++ src/components/hero.tsx
@@ -0,0 +1,20 @@
+export default function Hero(){
+	return(
+		<>
+
+<main className='flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-20'>
+  <h1 className='mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-800 sm:text-7xl font-sans'>
+	Strong
+	<span className='relative whitespace-nowrap text-[#3290EE] noto-sans'>
+	  <span className='relative font-sans'>password</span>
+	</span>{' '}
+	generator
+  </h1>
+
+  <p className='mx-auto mt-12 max-w-xl text-lg text-slate-700 leading-7 font-sans'>
+  This tool can Create highly secure and unique passwords that are impossible to crack or guess. Simply choose your desired password criteria and copy and paste the generated password for ultimate protection!
+  </p>
+</main>
+		</>
+	)
+}
---
+++ src/components/password.tsx
@@ -0,0 +1,324 @@
+'use client';
+
+import { Button } from "./ui/button";
+import { Input } from "@/components/ui/input"
+import { useLocalStorage } from "@/hooks/use-local-storage";
+import { useEffect } from "react";
+import { useState, useCallback } from "react";
+import { useCopy } from '@/hooks/use-copy';
+import { getSecureRandomInt } from '@/helpers/crypto';
+import { capitalizeString } from '@/helpers/string';
+import zxcvbn from 'zxcvbn';
+import clsx from "clsx";
+import {
+    FaRegCopy,
+    FaArrowRotateLeft,
+    FaCheck,
+    FaRegEye,
+    FaRegEyeSlash,
+  } from 'react-icons/fa6'
+
+import { wordlist } from '@/data/wordlist';
+
+const WORDLIST = wordlist;
+
+import styles from './password.module.css';
+
+
+export default function Password(){
+    const [activeTab, setActiveTab] = useLocalStorage<'normal' | 'diceware'>(
+        'pswd-active-tab',
+        'normal',
+      );
+      const { copy, copying } = useCopy();
+      const [showPassword, setShowPassword] = useLocalStorage(
+        'pswd-show-password',
+        true,
+      );
+    
+      const [password, setPassword] = useState('');
+      const [num,setnum] = useState(6)
+      const [length, setLength] = useLocalStorage('pswd-length', 12);
+      const [includeUpper, setIncludeUpper] = useLocalStorage(
+        'pswd-include-upper',
+        true,
+      );
+      const [includeLower, setIncludeLower] = useLocalStorage(
+        'pswd-include-lower',
+        true,
+      );
+      const [includeNumbers, setIncludeNumbers] = useLocalStorage(
+        'pswd-include-numbers',
+        true,
+      );
+      const [includeSymbols, setIncludeSymbols] = useLocalStorage(
+        'pswd-include-symbols',
+        true,
+      );
+      const [excludeSimilar, setExcludeSimilar] = useLocalStorage(
+        'pswd-exclude-similar',
+        false,
+      );
+      const [customSymbols, setCustomSymbols] = useLocalStorage(
+        'pswd-custom-symbols',
+        '',
+      );
+      const [excludeSymbols, setExcludeSymbols] = useLocalStorage(
+        'pswd-exclude-symbols',
+        '',
+      );
+    
+      const [wordCount, setWordCount] = useLocalStorage('pswd-word-count', 6);
+      const [separator, setSeparator] = useLocalStorage('pswd-separator', 'space');
+      const [capitalize, setCapitalize] = useLocalStorage('pswd-capitalize', false);
+    
+      const UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      const LOWERCASE = 'abcdefghijklmnopqrstuvwxyz';
+      const NUMBERS = '0123456789';
+      const SYMBOLS = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+    
+      const SIMILAR_CHARACTERS = 'Il1O0';
+    
+      const generatePassword = useCallback(() => {
+        if (activeTab === 'normal') {
+          let characterSet = '';
+    
+          if (includeUpper) characterSet += UPPERCASE;
+          if (includeLower) characterSet += LOWERCASE;
+          if (includeNumbers) characterSet += NUMBERS;
+          if (includeSymbols) characterSet += SYMBOLS;
+    
+          if (customSymbols) {
+            characterSet += customSymbols;
+          }
+    
+          let toExclude = '';
+    
+          if (excludeSimilar) {
+            toExclude += SIMILAR_CHARACTERS;
+          }
+    
+          if (excludeSymbols) {
+            toExclude += excludeSymbols;
+          }
+    
+          if (toExclude) {
+            const regex = new RegExp(`[${toExclude}]`, 'g');
+    
+            characterSet = characterSet.replace(regex, '');
+          }
+    
+          if (characterSet.length === 0) {
+            setPassword('');
+            return;
+          }
+    
+          const passwordCharacters = [];
+          const charsetLength = characterSet.length;
+    
+          for (let i = 0; i < length; i++) {
+            const randomIndex = getSecureRandomInt(charsetLength);
+            passwordCharacters.push(characterSet[randomIndex]);
+          }
+    
+          const newPassword = passwordCharacters.join('');
+          setPassword(newPassword);
+        } else {
+          if (WORDLIST.length === 0) {
+            alert('Wordlist is empty. Please provide a valid wordlist.');
+            return;
+          }
+    
+          const words = [];
+          const wordlistLength = WORDLIST.length;
+    
+          for (let i = 0; i < wordCount; i++) {
+            const index = getSecureRandomInt(wordlistLength);
+            const word = WORDLIST[index];
+    
+            words.push(capitalize ? capitalizeString(word) : word);
+          }
+    
+          setPassword(
+            words.join(
+              separator === 'space' ? ' ' : separator === 'dash' ? '-' : '',
+            ),
+          );
+        }
+      }, [
+        includeUpper,
+        includeLower,
+        includeNumbers,
+        includeSymbols,
+        length,
+        wordCount,
+        activeTab,
+        separator,
+        excludeSimilar,
+        customSymbols,
+        capitalize,
+        excludeSymbols,
+      ]);
+    
+      useEffect(() => {
+        generatePassword();
+      }, [activeTab, generatePassword]);
+    
+      const [strength, setStrength] = useState(0);
+      const strenthColor = [
+        'transparent',
+        '#ef4444',
+        '#f97316',
+        '#eab308',
+        '#65a30d',
+        '#22c55e',
+      ][strength];
+    
+      useEffect(() => {
+        if (password) {
+          const result = zxcvbn(password);
+          setStrength(result.score + 1);
+        } else {
+          setStrength(0);
+        }
+      }, [password]);
+    
+    
+    return(
+<>
+<div className=" flex min-h-screen flex-col text-[#bdb3b3] items-center justify-center text-center gap-12 p-8">
+
+<div className=" relative  max-w-screen-md  mx-auto rounded-xl flex w-full flex-col items-center text-center space-y-3 p-5 pb-3 sm:px-0 scroll-smooth">
+
+<Input readOnly 
+       className=" bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 text-black mx-auto transition cursor-copy border items-center text-center py-2 pr-10" 
+       type={showPassword ? 'text' : 'password'}
+       value={password}
+       width={30}
+       height={30}
+       />
+  <button
+    className={clsx(
+      "absolute inset-y-0 right-3 mx-auto flex  w-8 items-center justify-center rounded-md transition-all  text-center"
+    )}
+    onClick={() => copy(password)}
+  >
+    {copying ? <FaCheck /> : <FaRegCopy />}
+  </button>
+</div>
+<Button 
+className=" mx-auto flex items-center justify-center rounded-md transition-all  text-center px-4 bg-slate-700 text-white hover:bg-orange-500"
+onClick={() => generatePassword()}
+>
+    Generate new password
+</Button>
+
+<div className="  flex justify-between  mt-1 text-2xl min-h-14 items-center  leading-6 font-bold border-solid border-2 border-gray-600  px-2 py-2">
+{activeTab === 'normal' && (
+<>
+<label htmlFor="lenth" className=" flex  p-0  text-black gap-1 box-border text-base leading-6 font-bold text-center  items-center mx-2 noto-sans">password length :</label>
+<input
+id="length"
+max="100"
+min="6"
+type="number"
+value={length}
+className=" style flex  items-center text-center  border rounded-md border-s-orange-300 mx-2 noto-sans text-black"
+onChange={e => setLength(Number(e.target.value))}
+/>
+
+<input
+  max="100"
+ min="6"
+type="range"
+className=" style flex  items-center text-center mx-2 noto-sans"
+value={length}
+onChange={e => setLength(Number(e.target.value))}
+/>
+</>
+
+)}
+              
+</div>
+<div className="flex justify-between  text-2xl min-h-14 items-center  leading-6 font-bold">
+
+<label className=' flex items-center text-center noto-sans text-black mx-2'>
+  <input
+   checked={includeUpper}
+  type="checkbox"
+   className=" mx-2"
+  onChange={e => setIncludeUpper(e.target.checked)}
+    />
+   Include Uppercase Letters
+  </label>
+</div>
+<div className="flex justify-between  text-2xl min-h-14 items-center  leading-6 font-bold">
+
+<label className=' flex  items-center text-center noto-sans text-black '>
+<input
+checked={includeLower}
+type="checkbox"
+ className=" border-r-4 h-5 w-5 mx-2 cursor-pointer"
+onChange={e => setIncludeLower(e.target.checked)}
+/>
+Include Lowercase Letters
+</label>
+</div>
+
+<div className="flex justify-between text-2xl min-h-14 items-center  leading-6 font-bold  ">
+
+<label className="flex  items-center text-center justify-between gap-1 mx-2 noto-sans text-black" >
+                <input
+                  checked={includeNumbers}
+                  type="checkbox"
+                  onChange={e => setIncludeNumbers(e.target.checked)}
+                />
+                Include Numbers
+              </label>
+
+<label className="flex  items-center text-center justify-between gap-1 mx-2 noto-sans text-black" >
+<input
+  checked={includeSymbols}
+                  type="checkbox"
+                  onChange={e => setIncludeSymbols(e.target.checked)}
+                />
+                Include Symbols
+              </label>
+
+              <label className="flex  items-center text-center justify-between gap-1 mx-2 noto-sans text-black">
+                <input
+                  checked={excludeSimilar}
+                  type="checkbox"
+                  onChange={e => setExcludeSimilar(e.target.checked)}
+                />
+                Exclude Similar Characters (e.g., l, 1, O, 0)
+              </label>
+              </div>
+              <div className="flex justify-between  text-2xl min-h-14 items-center  leading-6 font-bold">
+        
+              <label htmlFor="excludeSymbols" className=" flex  items-center text-center justify-between gap-1 mx-1 noto-sans text-black">Exclude Symbols:
+                <input
+                  id="excludeSymbols"
+                  placeholder="e.g., /\?"
+                  type="text"
+                  className=" border-solid border-2 border-gray-600 focus-visible:text-gray-700 "
+                  value={excludeSymbols}
+                  onChange={e => setExcludeSymbols(e.target.value)}
+                />
+              </label>
+
+                <label htmlFor="customSymbols" className=" flex  items-center text-center justify-between gap-1 mx-2 noto-sans text-black">Custom Symbols:</label>
+                <input
+                  id="customSymbols"
+                  placeholder="e.g., @#$%"
+                  type="text"
+                  className=" border-solid border-2 border-gray-600 "
+                  value={customSymbols}
+                  onChange={e => setCustomSymbols(e.target.value)}
+                />
+              </div>
+              </div>
+
+</>
+    )
+}
